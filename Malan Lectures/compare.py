279a898150fad26ecf2a21903bb8f6dcfa820cc8
Laura <laura.e.vollmer@gmail.com>
2025-01-29 12:46:04 -0600

    Malan Lecture 1

---
+++ Malan Lectures/compare.py
@@ -0,0 +1,12 @@
+x = int(input("What's x?   "))
+y = int(input("What's y?   "))
+
+# boolean expression => do this or don't do this
+if x < y:
+    print("x is less than y")
+
+elif x > y:
+    print("x is greater than y")
+
+else:
+    print("x is equal to y")
