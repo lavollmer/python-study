c2d33a1dada2343ce447b7cbee2febcffef09084
Laura <laura.e.vollmer@gmail.com>
2025-01-30 13:36:25 -0600

    Finished Mario python file

--- Malan Lectures/Loops Problem/mario.py
+++ Malan Lectures/Loops Problem/mario.py
@@ -1,8 +1,10 @@
 def main():
-    print_column(3)
+    print_square(3)
 
-
-def print_column(height):
-    print("#\n" * height, end="")
+def print_square(size):
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
 
 main()
