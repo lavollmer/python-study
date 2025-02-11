a00a5839c1391703b90349513e3c4d645d41f902
Laura <laura.e.vollmer@gmail.com>
2025-02-11 11:29:23 -0600

    Updating the file with creating a dictionary

--- malan/file/studentsThree.py
+++ malan/file/studentsThree.py
@@ -8,6 +8,7 @@ with open("students.csv", "r") as file:
         students.append(student)
 
 # lambda function python that is anonymous
+# Can create as many anonymous functions as you like, use usually in passing into another function
 # function has no name, don't need name if you have only in one place
 # lambda is a anony function then parameter is student, don't use return keyword just code what you want
 for student in sorted(students, key=lambda student: student["name"]):
