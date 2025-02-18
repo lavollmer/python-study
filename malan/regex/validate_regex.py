2c69d5c911a79f06a68462a1f55a2d2e6af49d20
Laura <laura.e.vollmer@gmail.com>
2025-02-18 11:01:25 -0600

    Adding more regex notes

--- malan/regex/validate_regex.py
+++ malan/regex/validate_regex.py
@@ -1,6 +1,6 @@
 import re
 
-email = input("What's your email?").strip()
+email = input("What's your email? ").strip()
 
 # re.search(pattern,string), can take a bunch of special symbols
 # . any character except a newline
@@ -9,7 +9,13 @@ email = input("What's your email?").strip()
 # ? 0 or 1 repetition
 # {m} m reptitions
 # {m,n} m-n reptitions
-if re.search("@", email):
+
+# start state from left to right, stay in first state or transition to next state
+# did it end up in a final state
+# if we get stuck in the invalid state then invalid
+# escape character for a newline = backsplash to match on a dot 
+# r means to treat raw string (similiar to f string)
+if re.search(r".+@.+\.edu", email):
     print("valid")
 else:
     print("invalid")
