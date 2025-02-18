00d97108af46a73076a0ec0a707a8349e10beb58
Laura <laura.e.vollmer@gmail.com>
2025-02-18 14:52:53 -0600

    Update the validatematch file

--- malan/regex/validatematch.py
+++ malan/regex/validatematch.py
@@ -4,7 +4,6 @@ name = input("What's your name? ").strip()
 
 matches = re.search(r"^(.+), (.+)$", name)
 if matches:
-    last, first = matches.groups()
-    name = f"{first} {last}"
+    name = matches.group(2) + " " + matches.group(1)
 
 print(f"hello, {name}")
