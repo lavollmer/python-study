940813df332d5e9ae61e115f5b86dc2be3b5f8ab
Laura <laura.e.vollmer@gmail.com>
2025-02-19 11:49:05 -0600

    Updating the twitter.py

--- malan/regex/twitter.py
+++ malan/regex/twitter.py
@@ -1,13 +1,27 @@
 # goal extract username only
+import re
 
 url = input("URL: ").strip()
 
-# if url is a string comes with method replace - pass two arguments
-username = url.replace("https://twitter.com/","")
-print(f"{username}")
+# if url is a string comes with method removeprefix
+# username = url.removeprefix("https://twitter.com/")
+# print(f"Username: {username}")
 
 
 # what if user said http
 # what if had slash at end of username
 # what is no https
-# what if user uses www
+# what if user uses www (subdomain)
+# user experience - tolerant of all users input that are valid
+
+# re.sub is substituting
+# pattern
+# repl what do you want to replace it with
+# string what you want to replace
+# find and replace with regular expressions(regex)
+# need backsplash before . to escape it
+# ? means 0 or 1 the character before
+# need parentheses to group
+username = re.sub(r"^(https?://)?(www\.)?twitter\.com/","",url)
+
+print(f"Username: {username}")
