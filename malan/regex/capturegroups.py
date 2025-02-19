d637ba25f42a610ccc6d17ae123c07baa9100ee9
Laura <laura.e.vollmer@gmail.com>
2025-02-19 12:23:48 -0600

    Adding capture groups information and file

--- malan/regex/capturegroups.py
+++ malan/regex/capturegroups.py
@@ -1,14 +1,30 @@
+# Want to check valid phone number
+# Want to show what country they are calling from
+
 import re
 
 locations = {"+1":"United States and Canada", "+62": "Indonesia", "+505": "Nicaragua"}
 
 def main():
-    pattern = r"\+\d{1,3} \d{3}-\d{3}-\d{4}"
+    # \+ escaped with backsplash \ (expecting LITERAL character +)
+    # \d any kind of number between 0-9
+    # \d {1,3} between 1,3 numbers
+    # a space after
+    # exactly 3 numbers followed by a dash
+    # exactly 4 numbers at the end
+    # make a capture group by using parentheses () inside a regular expression group
+    # ?P<variable> this NAMES the group 
+    pattern = r"(?P<country_code>\+\d{1,3}) \d{3}-\d{3}-\d{4}"
     number= input("Number: ")
     
+    # stored number user's phone number
+    # searching for match in pattern
     match = re.search(pattern, number)
     if match:
-        print("Valid")
+        # use country_code as the KEY for the locations dictionary
+        country_code = match.group("country_code")
+        # user the key for the value
+        print(locations[country_code])
     else:
         print("Invalid")
 
