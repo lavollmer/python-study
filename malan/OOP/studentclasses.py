694ba44eab705b4bf57e155f66d9c1f830a5a3a6
Laura <laura.e.vollmer@gmail.com>
2025-02-24 08:40:55 -0600

    Created OOP file

---
+++ malan/OOP/studentclasses.py
@@ -0,0 +1,22 @@
+# keep all the related code togethed
+# raise exceptions - creating your own exceptions to alert the programmer there is an error
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError
+        # instance variable
+        self.name = name
+        self.house = house
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+    
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    # Student has been created
+    return Student(name, house)
+
+if __name__=="__main__":
+    main()
