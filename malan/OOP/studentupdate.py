d71a767c020d1f5df63ea91e60bedc983f94e4fb
Laura <laura.e.vollmer@gmail.com>
2025-02-24 08:35:41 -0600

    Adding python comments

--- malan/OOP/studentupdate.py
+++ malan/OOP/studentupdate.py
@@ -1,7 +1,15 @@
 # anytime you are using a class - you are creating Objects
 # classes are mutable but can make them immutable
 class Student:
-    ...
+    # instance methods
+    # designed the authors of python
+    # we are adding instance variables to objects
+    # blueprint always going to have a name and house but we can determine what it looks like
+    # init takes self gives to the current object that is created
+    def __init__(self, name, house):
+        # instance variable
+        self.name = name
+        self.house = house
 
 def main():
     student = get_student()
@@ -10,7 +18,9 @@ def main():
 def get_student():
     name = input("Name: ")
     house = input("House: ")
+    # classes come with methods(certain functions to determine behavior in a standard way)
     # passing arguments to the classes, will provide the ability to error check
+    # constructor call - instantiate a student object for me
     student = Student(name, house)
     return student
 
