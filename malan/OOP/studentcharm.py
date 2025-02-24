234d832ac598cf5461ed0f3e7c656e18d1164475
Laura <laura.e.vollmer@gmail.com>
2025-02-24 09:39:20 -0600

    Adding student charm py file

--- malan/OOP/studentcharm.py
+++ malan/OOP/studentcharm.py
@@ -1,6 +1,7 @@
 # property has a bunch of defense mechanisms
 # to prevent programmers from messing up functions
 # decorators functions that modify other functions
+# need function and variable to haver different names - add a little underscore
 class Student:
     def __init__(self, name, house):
         if not name:
@@ -8,28 +9,32 @@ class Student:
         # instance variable
         self.name = name
         # going to call setter method
-        self.house = house
+        self._house = house
+
     def __str__(self):
         return f"{self.name} from {self.house}"
-    
+
     # Getter
     # function for a class that gets a value
     @property
     def house(self):
-        return self.house
-    
+        return self._house
+
     # Setter
     # function for a class that sets a value
+    # name it the getter function plus .setter
     @house.setter
     def house(self, house):
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid House")
-        self.house = house
+        self._house = house
+
 
 def main():
     student = get_student()
     print(student)
-    
+
+
 def get_student():
     name = input("Name: ")
     house = input("House: ")
@@ -37,7 +42,7 @@ def get_student():
     return Student(name, house)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
 
 # https://docs.python.org/3/tutorial/classes.html
